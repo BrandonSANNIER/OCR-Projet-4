aafe95cdacf6069cf5a3fcea869f3ab26d37fddd
Brandon <brandonsannier@hotmail.fr>
2020-03-17 19:33:01 +0100

    Fixe full index responsive

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,6 +1,32 @@
 rellax = new Rellax('.rellax');
 btnComment = $("#btn_add_comment");
 
+const nbRoman = $('.list').length;
+var list = $('.list');
+console.log(list[0]);
+var current = 0;
+
+$('#next').on("click", () => {
+  current++;
+  if(current > nbRoman){
+    current = 0;
+  } else {
+    const test = list[1];
+    test.show();
+    test.css('display', 'block');
+    console.log("list[1]");
+  }
+});
+
+$('#prev').on("click", () => {
+  current--;
+  if(current < 0){
+    current = nbRoman - 1;
+  } else {
+    list[1].css('display', 'none');
+    console.log("test");
+  }
+});
 btnComment.on('click', () =>{
   console.log("show comment");
   $(".add_comment").show(500);
